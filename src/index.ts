93bb41edaacd81d332cf726f0c260365985be042
MayconPS <mayconsoares001@gmail.com>
2024-02-29 19:45:26 -0300

    firstcomit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+function somaEstranha(): string {
+    const resultado: string = "1" + "1";
+    return resultado;
+  }
+  
+  // Exemplo de uso
+  const resultadoDaSoma: string = somaEstranha();
+  console.log(resultadoDaSoma); 
+  
